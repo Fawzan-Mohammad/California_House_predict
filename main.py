40d6efc2bdd1ca43678cf99fdf03fd2d50c6b5ee
Fawzan Mohammed <fawzanmohammed38@gmail.com>
2026-08-29 12:21:28 +0000

    Resolve and update page not found issue

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("app.py", title="Home", icon=":material/home:", default=True),
-    st.Page("pages/about_us.py", icon=":material/info:", title ="About Us"),
-    st.Page("pages/terms_of_use.py", icon=":material/policy:", title="Terms Of Use"),
+    st.Page("pages/01_about_us.py", icon=":material/info:", title ="About Us"),
+    st.Page("pages/02_terms_of_use.py", icon=":material/policy:", title="Terms Of Use"),
     
 ])
 pg.run()
